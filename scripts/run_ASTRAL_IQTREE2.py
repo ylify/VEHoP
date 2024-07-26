54e92c96bc004d85a029d7f8cec71ca154225a00
Yunlong-Li <ylify@connect.ust.hk>
2024-07-26 09:33:16 +0800

    Update run_ASTRAL_IQTREE2.py

--- scripts/run_ASTRAL_IQTREE2.py
+++ scripts/run_ASTRAL_IQTREE2.py
@@ -9,29 +9,6 @@ cores = int(sys.argv[2])
 pwd = os.path.abspath('./')+'/'
 OGs = glob.glob(pwd+'genesortR/iqtree2/*.fa')
 models = {}
-fa_models = []
-with open(pwd+'/partition_data.new.txt.best_model.nex') as file:
-    for line in file:
-        if 'charset' not in line and 'OG' in line:
-            line = line.strip(',\n')
-            #Q.plant+I{0.144108}+R4{0.376699,0.180167,0.239364,0.865359,0.168895,2.13099,0.0709345,5.14675}: OG0005629{7.81994},
-            name = line.split(': ')[1].split('{')[0]
-            model = line.lstrip(' ').split(': ')[0]
-            models[name] = model
-            fa_models.append(pwd+'genesortR/iqtree2/'+name+'_pruned.fa@@'+model)
-
-def run_iqtree2(fa_model):
-    fa = fa_model.split('@@')[0]
-    model = fa_model.split('@@')[1]
-    cmd = 'iqtree2 -m '+model+' -B 1000 -T 1 -s '+fa
-    try:
-        if os.path.getsize(fa+'.contree') < 10:
-            raise Exception
-    except:
-        os.system(cmd)
-
-with multiprocessing.Pool(processes=cores) as pool:
-    pool.map(run_iqtree2, fa_models)
 
 def run_iqtree2_MFP(fa):
     cmd = 'iqtree2 -m MFP -B 1000 -T 1 -s '+fa
